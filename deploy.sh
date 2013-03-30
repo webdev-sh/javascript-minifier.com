60166cc135b0e2c1ab611f1fb916ace73893453e
Andrew Chilton <chilts@appsattic.com>
2013-03-30 20:10:10 +1300

    Make our assets minified on production

--- deploy.sh
+++ deploy.sh
@@ -16,8 +16,16 @@ echo
 
 # minimising assets
 echo "Minimising assets ..."
-# ToDo: js-min.pl public/s/js/ready.js > public/s/js/ready.min.js
-# ToDo: curl -X POST -s --data-urlencode 'input@public/s/css/style.css' http://cssminifier.com/raw > public/s/css/style.min.css
+curl        \
+    -X POST \
+    -s      \
+    --data-urlencode 'input@public/s/js/ready.js' \
+    http://javascript-minifier.com/raw > public/s/js/ready.min.js
+curl        \
+    -X POST \
+    -s      \
+    --data-urlencode 'input@public/s/css/style.css' \
+    http://cssminifier.com/raw > public/s/css/style.min.css
 echo
 
 # set up Nginx
