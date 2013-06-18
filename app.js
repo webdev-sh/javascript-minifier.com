b5cc4a8f0f52c0ea364e8a5b90bceb916f2a027b
Andrew Chilton <chilts@appsattic.com>
2013-06-19 07:43:44 +1200

    Fix up some bits related to cluster

--- app.js
+++ app.js
@@ -7,7 +7,6 @@
 // ----------------------------------------------------------------------------
 
 var cluster = require('cluster');
-var os      = require('os');
 
 "use strict";
 
@@ -20,24 +19,44 @@ var forks = process.env.NODE_ENV === 'development' ? 1 : 3;
 if( cluster.isMaster ) {
     process.title = 'parent.javascript-minifier.com';
     for( var i = 0; i < forks; i++ ) {
-        console.log('Starting child...');
+        console.log('MASTER: Starting child...');
         cluster.fork();
     }
 
-    cluster.on('exit', function(worker) {
-        console.log( 'Worker ' + worker.process.pid + ' died.' );
+    cluster.on('fork', function(worker) {
+        console.log('MASTER: Worker ' + worker.process.pid + ' has been forked');
+    });
+
+    cluster.on('online', function(worker) {
+        console.log('MASTER: Worker ' + worker.process.pid + ' has responded to say it is online');
+    });
+
+    cluster.on('listening', function(worker, address) {
+        console.log('MASTER: Worker ' + worker.process.pid + ' is now connected to ' + address.address + ':' + address.port);
+    });
+
+    cluster.on('disconnect', function(worker) {
+        console.log('MASTER: Worker ' + worker.process.pid + ' is now disconnecting (prior to dieing)');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        var exitCode = worker.process.exitCode;
+        console.log('MASTER: worker ' + worker.process.pid + ' died (' + exitCode + ') due to ' + signal + '.');
         cluster.fork();
     });
 }
 else {
+    // child
     process.title = 'child.javascript-minifier.com';
-    console.log('Worker %s started', process.pid);
+
+    console.log('WORKER: Worker %s started', process.pid);
+
     var server = require('./lib/server.js');
     var port = process.argv[2] || 3000;
-    server.listen(port, function() {
-        console.log('Worker %s listening on port %s', process.pid, port);
-    });
 
+    server.listen(port, function() {
+        console.log('WORKER: Worker %s listening on port %s', process.pid, port);
+    });
 }
 
 // ----------------------------------------------------------------------------
