d5c027c94bae651ccc0c2b5123d95a5c1f03ab77
Andrew Chilton <andychilton@gmail.com>
2019-03-01 09:44:06 +1300

    Move cron to be 1 min past the hour

--- deployer/cron.d
+++ deployer/cron.d
@@ -2,5 +2,5 @@ MAILTO=andychilton@gmail.com
 
 # mins hour dofm mnth dofw   user          command
 # ---- ---- ---- ---- ----   -----------   ----------------------------------------------------------------------------
-     0    *    *    *    *   chilts        find /var/cache/javascript-minifier.com/ -name '*.js' -mmin +1440 -delete
+     1    *    *    *    *   chilts        find /var/cache/javascript-minifier.com/ -name '*.js' -mmin +1440 -delete
 # ---- ---- ---- ---- ----   -----------   ----------------------------------------------------------------------------
